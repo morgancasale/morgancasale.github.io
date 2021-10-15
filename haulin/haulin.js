c58bd626804e70b2d1bc0aaf1695efd2ff38561b
Morgan Casale <morgan.casale@gmail.com>
2021-10-16 00:21:24 +0200

    id fix

--- haulin/haulin.js
+++ haulin/haulin.js
@@ -7,14 +7,14 @@ function init(){
     var profile = localStorage.getItem("profile");
     if(profile!=null){
         document.getElementById("signin").style.display = "none";
-        document.getElementsByClassName("table").style.display = "block";
+        document.getElementsById("table").style.display = "block";
     }
 }
 
 function checkCode(){
     if(document.getElementById("code_box").value == "626"){
-        document.getElementsByClassName("code").style.display = "none";
-        document.getElementsByClassName("message").style.display = "block";
+        document.getElementsById("table").style.display = "none";
+        document.getElementsById("message").style.display = "block";
         document.getElementById("message").innerHTML = "Đăng nhập thành công";
     }
 }
