20b3c48220fa5d52d24cd883b7e8cdb0f416edd7
Morgan Casale <morgan.casale@gmail.com>
2021-10-16 14:10:09 +0200

    fs

--- haulin/haulin.js
+++ haulin/haulin.js
@@ -21,15 +21,17 @@ function checkCode(){
     document.getElementById("message").innerHTML = message;
 }
 
+var clue_global;
 function searchMessage(code, name){
-    message = "No clue found!";
-    clues = JSON.parse(clues_data);
+    var message = "No clue found!";
+    var clues = JSON.parse(clues_data);
     clues.forEach(function(clue){
+        clue_global = clue.message;
         if(clue.code == code){
             var names = clue.names;
             names.forEach(function(clue_name){
                 if(clue_name == name){
-                    message = clue.message;
+                    message = clue_global;
                 }
             })
         }
