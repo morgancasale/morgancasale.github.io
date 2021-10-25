879cf280c7e5b3433ab16e1cc8917921c90f2d28
Morgan Casale <morgan.casale@gmail.com>
2021-10-25 12:02:35 +0200

    ifgg

--- haulin/clues.js
+++ haulin/clues.js
@@ -5,17 +5,17 @@ clues_data = `[
             "answer" : "Villa Becker",
             "perc" : "",
             "gps" : "45.052033, 7.700949",
-            "scs" : "Trova la stanza dove una volta vivevano statue e specchi qui venimmo battezzati",
+            "scs" : "Trova la stanza dove una volta vivevano statue e specchi qui venimmo battezzati.",
             "people" : [
                 {
                     "name" : "Alessandro Biral",
-                    "code" : "626",
-                    "atxt" : "L'assassino potrebbe essere Nada"
+                    "code" : "9678752",
+                    "atxt" : "L'assassino potrebbe essere Nada."
                 },
                 {
                     "name" : "Francesco Nada",
                     "code" : "645",
-                    "atxt" : "L'assassino potrebbe essere Biral"
+                    "atxt" : "L'assassino potrebbe essere Biral."
                 },
                 {
                     "name" : "Morgan Casale",
@@ -26,15 +26,15 @@ clues_data = `[
         },
         {
             "place" : "Superga",
-            "msg" : "Qui risiedono i fantasmi degli invincibili",
+            "msg" : "Qui risiedono i fantasmi degli invincibili.",
             "answer" : "Superga",
             "perc" : "",
             "gps" : "45.080404, 7.767198",
             "scs" : "Un dinosauro di legno ti indicherà la strada",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Riccardo Gallarato",
+                    "code" : "3325934",
                     "atxt" : ""
                 }
             ]
@@ -45,7 +45,7 @@ clues_data = `[
             "answer" : "Ultimo impero",
             "perc" : "",
             "gps" : "44.923135, 7.511560",
-            "scs" : "Al secondo piano dove gli artisti si cambiavano dietro pareti di vetro, ora rimangono solo più ricordi",
+            "scs" : "Al secondo piano dove gli artisti si cambiavano dietro pareti di vetro, ora rimangono solo più ricordi.",
             "people" : [
                 {
                     "name" : "",
@@ -60,11 +60,11 @@ clues_data = `[
             "answer" : "Palazzina di caccia di Stupinigi",
             "perc" : "",
             "gps" : "44.997381, 7.606905",
-            "scs" : "Oltre il palazzo superate il cancello che porta ad una strada morta, poi chiedete informazioni agli alberi",
+            "scs" : "Oltre il palazzo superate il cancello che porta ad una strada morta, poi chiedete informazioni agli alberi.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Lorenzo Callegari",
+                    "code" : "7636405",
                     "atxt" : ""
                 }
             ]
@@ -75,11 +75,31 @@ clues_data = `[
             "answer" : "Ponte del diavolo",
             "perc" : "",
             "gps" : "45.268090, 7.481308",
-            "scs" : "Portate la prova della vostra dedizione, i numeri sono tutto",
+            "scs" : "Portate la prova della vostra dedizione, i numeri sono tutto.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Stanislav Likhatskiy",
+                    "code" : "3445347",
+                    "atxt" : ""
+                },
+                {
+                    "name" : "Fabio Pistono",
+                    "code" : "6895670",
+                    "atxt" : ""
+                },
+                {
+                    "name" : "Alessandro Lo Giacco",
+                    "code" : "6895670",
+                    "atxt" : ""
+                },
+                {
+                    "name" : "Lorenzo Callegari",
+                    "code" : "4354577",
+                    "atxt" : ""
+                },
+                {
+                    "name" : "Lucia Crocetto",
+                    "code" : "4354577",
                     "atxt" : ""
                 }
             ]
@@ -90,7 +110,7 @@ clues_data = `[
             "answer" : "La morte",
             "perc" : "",
             "gps" : "45.011131, 7.726221",
-            "scs" : "Al piano di sopra dove su una poltrona un uomo avrà probabilmente tirato l'ultimo respiro",
+            "scs" : "Al piano di sopra dove su una poltrona un uomo avrà probabilmente tirato l'ultimo respiro.",
             "people" : [
                 {
                     "name" : "",
@@ -105,11 +125,11 @@ clues_data = `[
             "answer" : "masche",
             "perc" : "",
             "gps" : "45.084458, 7.751501",
-            "scs" : "Dopo aver salito quella lunga scalinata raggiungete la terrazza da cui potrete osservare la città immersa nella nebbia",
+            "scs" : "Dopo aver salito quella lunga scalinata raggiungete la terrazza da cui potrete osservare la città immersa nella nebbia.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Stanislav Likhatskiy",
+                    "code" : "5634784",
                     "atxt" : ""
                 }
             ]
@@ -120,11 +140,11 @@ clues_data = `[
             "answer" : "Ex acciaieria Lucchini",
             "perc" : "",
             "gps" : "45.146706, 7.749388",
-            "scs" : "Cercate quello che può sembrare il faro di questo luogo dimenticato",
+            "scs" : "Cercate quello che può sembrare il faro di questo luogo dimenticato.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Chiara Poggi",
+                    "code" : "4665744",
                     "atxt" : ""
                 }
             ]
@@ -135,12 +155,17 @@ clues_data = `[
             "answer" : "Il diavolo",
             "perc" : "",
             "gps" : "45.043610, 7.697581",
-            "scs" : "Ce una stanza verde con un camino, dove probabilmente i bambini una volta si radunavano",
+            "scs" : "Ce una stanza verde con un camino, dove probabilmente i bambini una volta si radunavano.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Loris Giannini",
+                    "code" : "2435434",
                     "atxt" : ""
+                },
+                {
+                    "name" : "Carlo Tabasso",
+                    "code" : "2435434",
+                    "atxt" : "Attenzione investigatore insider ci riportano informazioni che l'assassino potrebbe essere una ragazza del vostro gruppo, forse una silenziosa e insospettabile"
                 }
             ]
         },
@@ -150,12 +175,32 @@ clues_data = `[
             "answer" : "Mac Donald",
             "perc" : "",
             "gps" : "",
-            "scs" : "Ora è l'ora di emularli e rifocillarvi",
+            "scs" : "Ora è l'ora di emularli e rifocillarvi.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
-                    "atxt" : ""
+                    "name" : "Chiara Poggi",
+                    "code" : "4634523",
+                    "atxt" : "45.136471, 7.734862 Prossimo indizio: 4665744"
+                },
+                {
+                    "name" : "Adriano Briscese",
+                    "code" : "3546235",
+                    "atxt" : "45.117345, 7.709703 Prossimo indizio: 4665744"
+                },
+                {
+                    "name" : "Paolo Moscogiuri",
+                    "code" : "5349269",
+                    "atxt" : "45.112315, 7.644347 Prossimo indizio: 4665744"
+                },
+                {
+                    "name" : "Elena Ferrero",
+                    "code" : "2345237",
+                    "atxt" : "45.094021, 7.633389 Prossimo indizio: 4665744"
+                },
+                {
+                    "name" : "Valeria Piscopo",
+                    "code" : "9678752",
+                    "atxt" : "45.094021, 7.633389 Prossimo indizio: 4665744"
                 }
             ]
         },
@@ -165,11 +210,11 @@ clues_data = `[
             "answer" : "Ghostface",
             "perc" : "",
             "gps" : "45.100770, 7.716550",
-            "scs" : "Avvicinatevi a questa misteriosa torre persa nella boscaglia e cercate per il prossimo indizio",
+            "scs" : "Avvicinatevi a questa misteriosa torre persa nella boscaglia e cercate per il prossimo indizio.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Amico di stas",
+                    "code" : "3124696",
                     "atxt" : ""
                 }
             ]
@@ -180,11 +225,11 @@ clues_data = `[
             "answer" : "",
             "perc" : "",
             "gps" : "45.088216, 7.721847",
-            "scs" : "Un cavallo gigante vi indicherà la via",
+            "scs" : "Un cavallo gigante vi indicherà la via.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Stefano De Caria",
+                    "code" : "9678752",
                     "atxt" : ""
                 }
             ]
@@ -195,22 +240,7 @@ clues_data = `[
             "answer" : "Sleepy Hollow",
             "perc" : "",
             "gps" : "44.967304, 7.797260",
-            "scs" : "Cercate nei pressi del pozzo dove finirono molti dei nostri traditori",
-            "people" : [
-                {
-                    "name" : "",
-                    "code" : "",
-                    "atxt" : ""
-                }
-            ]
-        },
-        {
-            "place" : "Castello dei Mosetti",
-            "msg" : "Il cavaliere senza testa ha scelto questo villaggio come sua casa",
-            "answer" : "Sleepy Hollow",
-            "perc" : "",
-            "gps" : "44.967304, 7.797260",
-            "scs" : "Cercate nei pressi del pozzo dove finirono molti dei nostri traditori",
+            "scs" : "Cercate nei pressi del pozzo dove finirono molti dei nostri traditori.",
             "people" : [
                 {
                     "name" : "",
@@ -225,11 +255,11 @@ clues_data = `[
             "answer" : "Santo Graal",
             "perc" : "",
             "gps" : "45.076068, 7.753384",
-            "scs" : "Salite sulla torre più alta di quella villa nel bosco e godetevi la vista",
+            "scs" : "Salite sulla torre più alta di quella villa nel bosco e godetevi la vista.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Federico Croce",
+                    "code" : "4527814",
                     "atxt" : ""
                 }
             ]
@@ -240,11 +270,11 @@ clues_data = `[
             "answer" : "Occhio onniscente",
             "perc" : "",
             "gps" : "45.061518, 7.664944",
-            "scs" : "Al centro dell'occhio troverai la via",
+            "scs" : "Al centro dell'occhio troverai la via.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Sofia Cerasi",
+                    "code" : "4526190",
                     "atxt" : ""
                 }
             ]
@@ -258,8 +288,8 @@ clues_data = `[
             "scs" : "Placeholder",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Vittorio Colli",
+                    "code" : "5463764",
                     "atxt" : ""
                 }
             ]
@@ -270,11 +300,16 @@ clues_data = `[
             "answer" : "",
             "perc" : "",
             "gps" : "45.202852, 7.510330",
-            "scs" : "Se imparerete a muovervi come volpi riuscirete a trovare il vostro indizio dietro una delle nostre chiese, buona fortuna",
+            "scs" : "Se imparerete a muovervi come volpi riuscirete a trovare il vostro indizio dietro una delle nostre chiese, buona fortuna.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Stefano Lino",
+                    "code" : "5475789",
+                    "atxt" : ""
+                },
+                {
+                    "name" : "Joseph Laganà",
+                    "code" : "5654673",
                     "atxt" : ""
                 }
             ]
@@ -285,11 +320,11 @@ clues_data = `[
             "answer" : "La speranza",
             "perc" : "",
             "gps" : "45.086954, 7.548761",
-            "scs" : "Superato il cancello passando dal lato e poi addentratevi nel giardino della villa, qui troverete una rimessa e il vostro nuovo indizio",
+            "scs" : "Superato il cancello passando dal lato e poi addentratevi nel giardino della villa, qui troverete una rimessa e il vostro nuovo indizio.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Elena Ferrero",
+                    "code" : "4665744",
                     "atxt" : ""
                 }
             ]
@@ -300,11 +335,11 @@ clues_data = `[
             "answer" : "",
             "perc" : "",
             "gps" : "45.105980, 7.476528",
-            "scs" : "Seguite la via degli occhi dei tagliafuoco per raggiungere una torre dimenticato nel bosco",
+            "scs" : "Seguite la via degli occhi dei tagliafuoco per raggiungere una torre dimenticato nel bosco.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Valeria Piscopo",
+                    "code" : "4665744",
                     "atxt" : ""
                 }
             ]
@@ -315,11 +350,11 @@ clues_data = `[
             "answer" : "",
             "perc" : "",
             "gps" : "45.116339, 7.480388",
-            "scs" : "Al centro di quella casa antica troverete la via",
+            "scs" : "Al centro di quella casa antica troverete la via.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Anna Ramella",
+                    "code" : "4578954",
                     "atxt" : ""
                 }
             ]
@@ -330,7 +365,7 @@ clues_data = `[
             "answer" : "",
             "perc" : "",
             "gps" : "45.026691, 7.732361",
-            "scs" : "Raggiungete la cima di questo luogo dove molti anziani trovarono la loro fine nella solitudine del bosco",
+            "scs" : "Raggiungete la cima di questo luogo dove molti anziani trovarono la loro fine nella solitudine del bosco.",
             "people" : [
                 {
                     "name" : "",
@@ -345,7 +380,7 @@ clues_data = `[
             "answer" : "",
             "perc" : "",
             "gps" : "45.157620, 7.573697",
-            "scs" : "Nel cuore della Mandria troverete il prossimo indizio",
+            "scs" : "Nel cuore della Mandria troverete il prossimo indizio.",
             "people" : [
                 {
                     "name" : "",
@@ -355,16 +390,56 @@ clues_data = `[
             ]
         },
         {
-            "place" : "Orologio della Reggi di Venaria",
+            "place" : "Orologio della Reggia di Venaria",
+            "msg" : "In questa città regale uno zvc trovò il suo luogo di nascita",
+            "answer" : "Venaria",
+            "perc" : "",
+            "gps" : "45.135262, 7.625544",
+            "scs" : "Nel cuore della Casa Savoia c'è un grande orologio.",
+            "people" : [
+                {
+                    "name" : "Adriano Briscese",
+                    "code" : "4665744",
+                    "atxt" : ""
+                },
+                {
+                    "name" : "Paolo Moscogiuri",
+                    "code" : "4665744",
+                    "atxt" : ""
+                }
+            ]
+        },
+        {
+            "place" : "Ospedale abbandonato",
             "msg" : "",
             "answer" : "",
             "perc" : "",
-            "gps" : "45.135262, 7.625544",
-            "scs" : "Nel cuore della Casa Savoia ce un grande orologio",
+            "gps" : "45.031771, 7.710569",
+            "scs" : "Un bagno nella piscina di questo luogo dimenticato ci riporta a tempi passati.",
             "people" : [
                 {
-                    "name" : "",
-                    "code" : "",
+                    "name" : "Antonio Maria Pirozzi",
+                    "code" : "5675622",
+                    "atxt" : ""
+                },
+                {
+                    "name" : "Lucia Crocetto",
+                    "code" : "5675468",
+                    "atxt" : ""
+                }
+            ]
+        },
+        {
+            "place" : "Villa Moglia",
+            "msg" : "",
+            "answer" : "",
+            "perc" : "",
+            "gps" : "45.018574, 7.787314",
+            "scs" : "In questo teatro i bambini si divertivano a recitare prima che svanissero tutti nel nulla",
+            "people" : [
+                {
+                    "name" : "Francesco Nada",
+                    "code" : "3478356",
                     "atxt" : ""
                 }
             ]
