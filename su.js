b011e0d6f2e7a33d3eee5b88a960e5d926acef61
Morgan Casale <morgan.casale@gmail.com>
2023-06-02 22:30:13 +0200

    <asdklònaòslkd

--- su.js
+++ su.js
@@ -171,7 +171,19 @@ function in_Map(map, point){
     return result;
 }
 
+function notifyDisqualified(){
+    var xhttp = new XMLHttpRequest();
+    localStorage.setItem("disqualified", true);
+    var ch_id = "@faur_channel";
+    var text = player_name + " è stato squalificato! \r\n ";
+    text += "Alle coordinate \r\n " + position[1] + "," + position[0];
+    url = "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage?chat_id=" + ch_id + "&text=" + text;
+    xhttp.open("POST", url, true);
+    xhttp.send();
+}
+
 async function setDisqualified(){
+    notifyDisqualified();
     disqualified = "true";
     localStorage.setItem("disqualified", disqualified);
     document.body.style.backgroundColor = "#d50000";
@@ -186,8 +198,6 @@ async function setDisqualified(){
     }
     sendNotification("SQUALIFICATO E SEGNALATO!");
     sendTgMsg("SQUALIFICATO E SEGNALATO!");
-    
-    notifyDisqualified();
 
     await sleep(1000);
     if(!ios){
