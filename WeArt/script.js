37b202c004c419f2e549cc8d7826be606edc563d
Morgan Casale <morgan.casale@gmail.com>
2024-10-26 14:03:55 +0200

    test

---
+++ WeArt/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const sheetDataHandler = (sheetData) => {
+      console.log("sheet data: ", sheetData);
+      //ADD YOUR CODE TO WORK WITH sheetData ARRAY OF OBJECTS HERE
+    };
+  
+    // --==== QUERY EXAMPLES ====--
+    // --==== USE LETTERS FOR COLUMN NAMES ====--
+    //  'SELECT A,C,D WHERE D > 150'
+    //  'SELECT * WHERE B = "Potato"'
+    //  'SELECT * WHERE A contains "Jo"'
+    //  'SELECT * WHERE C = "active" AND B contains "Jo"'
+    //  "SELECT * WHERE E > date '2022-07-9' ORDER BY E DESC"
+  
+    getSheetData({
+      // sheetID you can find in the URL of your spreadsheet after "spreadsheet/d/"
+      sheetID: "1XZ1vGGTOhbiHAEu1_y0nLIKvzKmkNYa5DXyPHaD_pnE",
+      // sheetName is the name of the TAB in your spreadsheet (default is "Sheet1")
+      sheetName: "materials",
+      query: "SELECT A offset 1",
+      callback: sheetDataHandler,
+    });
+  });
