1868f7efe4817853b4491f376845784318e85564
Morgan Casale <morgan.casale@gmail.com>
2024-11-29 17:48:25 +0100

    test stella_maris

---
+++ Stella_Maris/test.js
@@ -0,0 +1,38 @@
+let service_uuid = '19b10000-e8f2-537e-4f6c-d104768a1214';
+let characteristic_uuid = '19b10001-e8f2-537e-4f6c-d104768a1214';
+
+let connectedDevices = [];
+
+async function connectToDevicesUntilCancel() {
+    try {
+        while (true) {
+            console.log("Opening pairing menu...");
+            
+            // Request a device through the pairing interface
+            const device = await navigator.bluetooth.requestDevice({
+                filters: [{
+                    services: [service_uuid]
+                }]
+            });
+
+            // Connect to the selected device
+            const server = await device.gatt.connect();
+            console.log(`Connected to device: ${device.name}`);
+
+            // Store the connected device
+            connectedDevices.push(device);
+        }
+    } catch (error) {
+        if (error.name === "NotFoundError") {
+            // Pairing menu was canceled by the user
+            console.log("Pairing menu canceled. Stopping device connection.");
+        } else {
+            console.error("An error occurred:", error);
+        }
+    }
+
+    console.log("Final list of connected devices:", connectedDevices);
+}
+
+// Start the process
+connectToDevicesUntilCancel();
