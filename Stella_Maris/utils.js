6865a99a5caf3c281e3203f479e507aac1678874
Morgan Casale <morgan.casale@gmail.com>
2025-01-17 14:23:52 +0100

    fix spaces

--- Stella_Maris/utils.js
+++ Stella_Maris/utils.js
@@ -10,14 +10,14 @@ async function withTimeout(promise, timeout) {
 
 function fix_dev_name(dev_name){
     if(dev_name.includes(" ")){
-        dev_name = dev_name.replace(" ", "_");
+        dev_name = dev_name.replaceAll(" ", "_");
     }
     return dev_name;
 }
 
 function unfix_dev_name(dev_name){
     if(dev_name.includes("_")){
-        dev_name = dev_name.replace("_", " ");
+        dev_name = dev_name.replaceAll("_", " ");
     }
     return dev_name;
 }
